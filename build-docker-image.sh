9bce4ba309734b845b45203f0854e788514a4f27
Sami Siren <siren@foofactory.fi>
2017-02-20 15:37:45 +0200

    use builder

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 ORG=hsldevcom
 DOCKER_IMAGE=opentripplanner-data-container
 
@@ -9,7 +10,10 @@ DOCKER_IMAGE=opentripplanner-data-container
 #DOCKER_AUTH=
 
 # Build image
-docker build --tag="$ORG/$DOCKER_IMAGE:$DOCKER_TAG" .
+docker build --tag="$ORG/$DOCKER_IMAGE:builder" -f Dockerfile.builder .
+mkdir target
+docker run --rm --entrypoint tar hsldevcom/opentripplanner-data-container -c /opt/opentripplanner-data-container/webroot|tar x -C target .
+docker build --tag="$ORG/$DOCKER_IMAGE:$DOCKER_TAG" -f Dockerfile .
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_AUTH
 docker push $ORG/$DOCKER_IMAGE:$DOCKER_TAG
 docker tag -f $ORG/$DOCKER_IMAGE:$DOCKER_TAG $ORG/$DOCKER_IMAGE:latest
