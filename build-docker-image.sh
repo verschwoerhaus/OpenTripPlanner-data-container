d2c228a98291193c7e6a344d42c65836c450b1ba
Sami Siren <siren@foofactory.fi>
2017-04-21 12:39:02 +0300

    prebuild graphs inside data container

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -9,10 +9,30 @@ DOCKER_IMAGE=opentripplanner-data-container
 #DOCKER_USER=
 #DOCKER_AUTH=
 
-# Build image
+# Build data with builder
+rm -rf target build
 docker build --tag="$ORG/$DOCKER_IMAGE:builder" -f Dockerfile.builder .
 mkdir target
-docker run --rm --entrypoint tar hsldevcom/opentripplanner-data-container:builder -c /opt/opentripplanner-data-container/webroot|tar x -C target 
+docker run --rm --entrypoint tar hsldevcom/opentripplanner-data-container:builder -c /opt/opentripplanner-data-container/webroot|tar x -C target
+
+mkdir build
+
+##prebuild graph
+function prebuild () {
+  NAME=$1
+  mkdir build/$NAME
+  unzip -j target/opt/opentripplanner-data-container/webroot/router-$NAME.zip  -d build/$NAME/
+  VERSION=`docker run --rm --entrypoint /bin/bash hsldevcom/opentripplanner:prod  -c "java -jar otp-*-shaded.jar --version"|grep commit|cut -d' ' -f2`
+  docker run -v `pwd`/build:/opt/opentripplanner/graphs --rm --entrypoint /bin/bash hsldevcom/opentripplanner:prod  -c "java -Xmx6g -jar otp-*-shaded.jar --build graphs/$NAME"
+  cd build/
+  zip graph-$NAME-$VERSION.zip $NAME/router-config.json $NAME/Graph.obj
+  cd ..
+}
+
+prebuild 'hsl'
+prebuild 'waltti'
+prebuild 'finland'
+
 docker build --tag="$ORG/$DOCKER_IMAGE:$DOCKER_TAG" -f Dockerfile .
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_AUTH
 docker push $ORG/$DOCKER_IMAGE:$DOCKER_TAG
