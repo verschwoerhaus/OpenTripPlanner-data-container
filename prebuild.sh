63628d872ffa4b2ae3c96e3247d1929e32b42e38
Vesa Meskanen <vesa.meskanen@cgi.com>
2017-05-23 08:14:39 +0300

    Trigger new build

--- prebuild.sh
+++ prebuild.sh
@@ -15,6 +15,8 @@ DOCKER_TAG=${DOCKER_TAG:-$TRAVIS_BUILD_ID}
 DOCKER_IMAGE=$ORG/$CONTAINER-$ROUTER_NAME
 DOCKER_BUILDER_IMAGE=$DOCKER_IMAGE:builder
 
+echo "***Build ID:"$DOCKER_TAG
+
 echo "*** Fetching OSM data"
 curl http://dev.hsl.fi/osm.finland/finland.osm.pbf -o finland.osm.pbf &
 curl http://dev.hsl.fi/osm.hsl/hsl.osm.pbf -o hsl.osm.pbf &
