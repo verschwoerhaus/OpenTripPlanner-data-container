76aa069c4c26a1763bb3d5a1f70304e8a4dcd5d6
Sami Siren <siren@foofactory.fi>
2017-06-27 16:07:18 +0300

    add hsl hack task

---
+++ task/hslHackTask.js
@@ -0,0 +1,49 @@
+const exec = require('child_process').exec;
+const gutil = require('gulp-util');
+const col = gutil.colors;
+const {dataToolImage} = require('../config.js');
+
+
+/**
+ * Make router data ready for inclusion in data container.
+ */
+module.exports = function(){
+
+
+  const p = new Promise((resolve, reject) => {
+
+    const lastLog=[];
+    const cmd = `set -e
+  unzip -o /data/filter/gtfs/HSL.zip stop_times.txt
+  # TODO: Check that the line is in expected format
+  # Needed in order to get rid of 'shape_distance_travelled'
+  # this field is not currently available in shapes.txt. OpenTripPlanner needs it to be.
+  cut --complement -f 9 -d, stop_times.txt > stop_times.new
+  mv stop_times.new stop_times.txt
+  zip -f /data/filter/gtfs/HSL.zip stop_times.txt
+  rm stop_times.txt`;
+    const fullCommand = `docker run --rm -v $(pwd):/data ${dataToolImage} bash -c "${cmd}"`;
+    console.log(fullCommand);
+    const hslHack = exec(fullCommand, {maxBuffer:1024*1024*8});
+
+    hslHack.on('exit', function(c){
+      if(c===0) {
+        process.stdout.write(col.green('HSL Hack SUCCESS\n'));
+        resolve();
+      } else {
+        const e = lastLog.join('');
+        reject(e);
+        process.stdout.write(col.red(`HSL Hack FAILED (${c})\n${e}\n`));
+      }
+    });
+
+    hslHack.stdout.on('data', function (data) {
+      console.log(data.toString());
+      lastLog.push(data.toString());
+      if(lastLog.length===20) {
+        delete lastLog[0];
+      }
+    });
+  });
+  return p;
+};
