590b0f5f1cffbdc40f71a6a5263582949446483e
Sami Siren <siren@foofactory.fi>
2017-06-27 14:34:31 +0300

    make fnction available

--- util.js
+++ util.js
@@ -2,9 +2,27 @@ const JSZip = require('JSZip');
 const fs = require('fs');
 const globby = require('globby');
 
+
+const zipWithGlob = (zipFile, glob, cb) => {
+
+  return globby(glob).then(paths => {
+    const zip = new JSZip();
+    paths.forEach(file => {
+      zip.file(file.split('/').pop(), fs.createReadStream(file));
+    });
+
+    zip.generateNodeStream({streamFiles:true,compression: 'DEFLATE',compressionOptions : {level:6}})
+      .pipe(fs.createWriteStream(zipFile))
+      .on('finish', (err) => {
+        cb(err);
+      });
+  });
+};
+
+
 module.exports= {
   zipDir: (zipFile, dir, cb) => {
-    this.zipWithGlob(zipFile, [`${dir}/**/*`], cb);
+    zipWithGlob(zipFile, [`${dir}/*`], cb);
   },
 
   /**
@@ -13,20 +31,6 @@ module.exports= {
    * glob pattern array
    * cb function to call when done
    */
-  zipWithGlob: (zipFile, glob, cb) => {
-
-    return globby(glob).then(paths => {
-      const zip = new JSZip();
-      paths.forEach(file => {
-        zip.file(file.split('/').pop(), fs.createReadStream(file));
-      });
-
-      zip.generateNodeStream({streamFiles:true,compression: 'DEFLATE',compressionOptions : {level:6}})
-        .pipe(fs.createWriteStream(zipFile))
-        .on('finish', (err) => {
-          cb(err);
-        });
-    });
-  },
+  zipWithGlob,
   routerDir: (config) => `router-${config.id}`
 };
