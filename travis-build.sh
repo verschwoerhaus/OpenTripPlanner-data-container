85fc9fe517127a0c9d613a0a8bf6cd78d86c4cf9
Sami Siren <siren@foofactory.fi>
2017-06-29 09:21:06 +0300

    build both containers

--- travis-build.sh
+++ travis-build.sh
@@ -8,15 +8,27 @@ set +e
 #DOCKER_USER // dockerhub credentials
 #DOCKER_AUTH
 
+ORG=${ORG:-hsldevcom}
+DOCKER_TAG=${TRAVIS_BUILD_ID:-latest}
+DOCKER_IMAGE=$ORG/otp-data-builder:${DOCKER_TAG}
+
+echo Building otp-data-builder: $DOCKER_IMAGE
+
+docker build  --tag=$DOCKER_IMAGE -f Dockerfile .
+docker login -u $DOCKER_USER -p $DOCKER_AUTH
+docker push $DOCKER_IMAGE
+
+echo Build completed
+
 cd otp-data-tools
 
 ORG=${ORG:-hsldevcom}
 DOCKER_TAG=${TRAVIS_BUILD_ID:-latest}
 DOCKER_IMAGE=$ORG/otp-data-tools:${DOCKER_TAG}
 
-echo Building image: $DOCKER_IMAGE
+echo Building otp-data-tools: $DOCKER_IMAGE
 
-docker build  --tag=$DOCKER_IMAGE -f Dockerfile.otp-data-tools .
+docker build  --tag=$DOCKER_IMAGE -f Dockerfile .
 docker login -u $DOCKER_USER -p $DOCKER_AUTH
 docker push $DOCKER_IMAGE
 
