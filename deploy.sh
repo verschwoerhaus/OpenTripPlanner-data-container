960c83cffb87a5759d79b720d600df3f9e092ecc
Sami Siren <siren@foofactory.fi>
2017-07-13 13:43:17 +0300

    push image per day & also deploy to prod

--- deploy.sh
+++ deploy.sh
@@ -8,11 +8,13 @@
 set -e
 
 ROUTER_NAME=${1:-hsl}
+DATE=`date +"%Y-%m-%d"`
 
 ORG=${ORG:-hsldevcom}
 CONTAINER=opentripplanner-data-container
 DOCKER_IMAGE=$ORG/$CONTAINER-$ROUTER_NAME
 DOCKER_TEST_IMAGE=$DOCKER_IMAGE:test
+DOCKER_DATE_IMAGE=$DOCKER_IMAGE:$DATE
 DOCKER_LATEST_IMAGE=$DOCKER_IMAGE:latest
 DOCKER_PROD_IMAGE=$DOCKER_IMAGE:prod
 
@@ -23,10 +25,14 @@ echo "*** Testing $ROUTER_NAME..."
 
 echo "*** $ROUTER_NAME tests passed"
 
-echo "*** Deploying $ROUTER_NAME"
 docker login -u $DOCKER_USER -p $DOCKER_AUTH
+docker tag $DOCKER_TEST_IMAGE $DOCKER_DATE_IMAGE
+echo "*** Pushing $DOCKER_DATE_IMAGE"
+docker push $DOCKER_DATE_IMAGE
 docker tag $DOCKER_TEST_IMAGE $DOCKER_LATEST_IMAGE
+echo "*** Pushing $DOCKER_LATEST_IMAGE"
 docker push $DOCKER_LATEST_IMAGE
-#docker tag $DOCKER_TEST_IMAGE $DOCKER_PROD_IMAGE
-#docker push $DOCKER_PROD_IMAGE
+docker tag $DOCKER_TEST_IMAGE $DOCKER_PROD_IMAGE
+echo "*** Pushing $DOCKER_PROD_IMAGE"
+docker push $DOCKER_PROD_IMAGE
 echo "*** Deployed $ROUTER_NAME"
