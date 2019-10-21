9d70cb7ffa3986ca429c3a1533e09d05ee4fb734
Maximilian Richt <maxi@richt.name>
2019-10-21 23:04:26 +0200

    test: stop only current router container

--- test.sh
+++ test.sh
@@ -16,12 +16,8 @@ function shutdown() {
 }
 
 echo "Making sure there are no old test containers or image available"
-docker stop otp-data-finland || true
-docker stop otp-finland || true
-docker stop otp-data-waltti || true
-docker stop otp-waltti || true
-docker stop otp-data-hsl || true
-docker stop otp-hsl || true
+docker stop otp-data-$ROUTER_NAME || true
+docker stop otp-$ROUTER_NAME || true
 docker rmi --force $DOCKER_IMAGE || true
 cd data/build/$ROUTER_NAME
 echo "Building data-container image..."
